2b96b505120ff9fd0328bcf6371e5bcc9bc6c744
stefwind <stef.wind@xs4all.nl>
2014-06-04 12:38:39 +0200

    V0.1

---
+++ PML.R
@@ -0,0 +1,8 @@
+library(caret)
+library(kernlab)
+data(spam)
+inTrain <- createDataPartition(y=spam$type,
+                               p=0.75, list=FALSE)
+training <- spam[inTrain,]
+testing <- spam[-inTrain,]
+dim(training)
